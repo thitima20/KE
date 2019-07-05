5d226e0a2dfeebdb8a37f1d0ec54a49ef5e644ad
thitima20 <52314903+thitima20@users.noreply.github.com>
2019-07-05 14:06:01 +0700

    Add files via upload
    โปรแกรมแรก

---
+++ keshop1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("ke shop")
+print("------------------")
+print("1.water X 1 10THB = 10THB")
+print("------------------")
+print("2.lay X 3 30THB = 30THB")
+print("------------------")
+print("3.icecream X 3 10THB = 30THB")
+print("-----------------------------------------")
+print("Total                      =70THB")
+
+
+# In[2]:
+
+
+print(10)
+
+
+# In[3]:
+
+
+print(type(10))
+
+
+# In[5]:
+
+
+print(type("hello"))
+
+
+# In[7]:
+
+
+print(type(True))
+
+
+# In[8]:
+
+
+print(type(10.3))
+
+
+# In[9]:
+
+
+print(type(1j))
+
+
+# In[10]:
+
+
+print((10+3.5))
+
+
+# In[ ]:
+
+
+
+
