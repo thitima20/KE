5896f88227ba241e28feaaf059a3358beb287e46
thitima20 <52314903+thitima20@users.noreply.github.com>
2019-09-05 12:01:48 +0700

    Add files via upload

---
+++ Temp.py
@@ -0,0 +1,4 @@
+Heat = float(input("Heat Cํ :"))
+Heat_shift_Cํ_to_Aํ = (9/5*Heat)+32
+Heat_shift_Cํ_to_Oํ = (Heat + 273.15)
+print("Heat Aํ = ", Heat_shift_Cํ_to_Aํ,"      " "Heat Oํ = " ,Heat_shift_Cํ_to_Oํ)
