4350d582b8bbee11e3788abd8353226f49041500
thitima20 <52314903+thitima20@users.noreply.github.com>
2019-09-06 22:49:55 +0700

    Add files via upload

---
+++ Triangle3.py
@@ -0,0 +1,10 @@
+max = 0
+i = 3
+sum = 0
+for i in range(i):
+    if angle > max:
+        max = angle
+    angle = int(input("enter angle :"))
+    sum = sum + angle
+print("sum :", sum)
+print("hypotenuse", max)
